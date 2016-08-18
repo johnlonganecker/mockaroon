3f32269c70932e384859c313dde6457cb6b58a09
johnlonganecker <johnlonganecker@gmail.com>
2016-08-18 09:25:30 -0700

    added in a config file to control routes

--- main.go
+++ main.go
@@ -2,62 +2,115 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
+
+	"gopkg.in/yaml.v2"
 
 	"github.com/gorilla/mux"
 )
 
-type endpoint struct {
-	Hosts   []string          `json:"hosts"`
-	Paths   []string          `json:"paths"`
-	Status  int               `json:"status"`
-	Headers map[string]string `json:"headers"`
-	Methods []string          `json:"methods"`
-	Body    string            `json:"body"`
+type Endpoint struct {
+	Paths   []string            `json:"paths"`
+	Status  int                 `json:"status"`
+	Headers []map[string]string `json:"headers"`
+	Methods []string            `json:"methods"`
+	Body    string              `json:"body"`
 }
 
-func (e endpoint) HandleHTTP(w http.ResponseWriter, req *http.Request) {
-	for key, value := range e.Headers {
-		w.Header().Set(key, value)
+type Config struct {
+	Port      string     `json:"Port"`
+	Endpoints []Endpoint `json:"Endpoint"`
+}
+
+func (e Endpoint) HandleHTTP(w http.ResponseWriter, req *http.Request) {
+	for _, header := range e.Headers {
+		for key, value := range header {
+			w.Header().Set(key, value)
+		}
 	}
 	w.WriteHeader(e.Status)
 	w.Write([]byte(e.Body))
+}
+
+func (c *Config) LoadConfigFile(filepath string) error {
+
+	var data []byte
+
+	data, err := LoadFile(filepath)
+	if err != nil {
+		return err
+	}
+
+	if err := Unmarshal(c, data); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func LoadFile(filepath string) ([]byte, error) {
+
+	data, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return data, nil
+}
+
+func Unmarshal(c *Config, data []byte) error {
+
+	// unmarshal yaml
+	err := yaml.Unmarshal(data, c)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func main() {
 
 	port := ":8080"
 
+	config := Config{}
+
+	err := config.LoadConfigFile("sample-config.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	// load in config file
 
-	// create endpoints
-	endpoints := make([]endpoint, 1)
+	// create Endpoints
+	//endpoints := config.Endpoints
 
-	endpoints = append(endpoints, endpoint{
-		Paths: []string{"/bob", "/joe", "/moe/joe"},
-		Headers: map[string]string{
-			"Content-Type": "application/json",
-		},
-		Methods: []string{"GET", "POST"},
-		Body:    "{\"ok\": 10}",
-	})
+	//endpoints = append(endpoints, Endpoint{
+	//Paths: []string{"/bob", "/joe", "/moe/joe"},
+	//Headers: map[string]string{
+	//"Content-Type": "application/json",
+	//},
+	//Methods: []string{"GET", "POST"},
+	//Body:    "{\"ok\": 10}",
+	//})
 
-	endpoints = append(endpoints, endpoint{
-		Paths: []string{"/okok"},
-		Headers: map[string]string{
-			"Content-Type": "application/json",
-		},
-		Methods: []string{"GET", "POST"},
-		Body:    "{\"ok\": 10, \"b\": {\"c\": [1,2,3,4,5]}}",
-	})
+	//endpoints = append(endpoints, Endpoint{
+	//Paths: []string{"/okok"},
+	//Headers: map[string]string{
+	//"Content-Type": "application/json",
+	//},
+	//Methods: []string{"GET", "POST"},
+	//Body:    "{\"ok\": 10, \"b\": {\"c\": [1,2,3,4,5]}}",
+	//})
 
 	// create mux router
 	muxRouter := mux.NewRouter()
 
-	for _, endpoint := range endpoints {
+	for _, endpoint := range config.Endpoints {
 		for _, path := range endpoint.Paths {
 			muxRouter.HandleFunc(path, endpoint.HandleHTTP).Methods(endpoint.Methods...)
-			fmt.Println("adding route " + host + port + path)
+			fmt.Println("adding route " + port + path)
 		}
 	}
 
